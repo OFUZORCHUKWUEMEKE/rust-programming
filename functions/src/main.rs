c99965dbb1c97a6412729f6332ac9412561e21fa
Ofuzor chukwuemeke <uniben2018@gmail.com>
2024-05-08 23:43:14 +0100

    functions

---
+++ functions/src/main.rs
@@ -0,0 +1,47 @@
+fn main() {
+    println!("Hello, world!");
+    // another_function();
+
+    // another_function(5)
+    print_labeled_measurement(5,'h)
+}
+
+// fn another_function(){
+//     let var_name = println!("Another function.");
+// }
+
+
+// fn another_function(x : i32){
+//     println!("The value of x is: {x}");
+// }
+
+fn print_labeled_measurement(value:i32,unit_label:char){
+    println!("the measurement is : {value}{unit_label}")
+
+}
+
+
+
+
+
+// function with return values 
+
+// fn five()->i32{
+//     5
+// }
+
+// func main(){
+//     let x = five()
+//     println!(The value of x is: {x};)
+// }
+
+
+// fn main() {
+//     let x = plus_one(5);
+
+//     println!("The value of x is: {x}");
+// }
+
+// fn plus_one(x: i32) -> i32 {
+//     x + 1
+// }
