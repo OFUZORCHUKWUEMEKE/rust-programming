a20a8b6b6613235c398db523eb08086ae37cd4cc
Ofuzor chukwuemeke <uniben2018@gmail.com>
2024-05-08 22:57:11 +0100

    dataTypes

---
+++ dataTypes/src/main.rs
@@ -0,0 +1,67 @@
+
+fn main() {
+
+    // datatypes in the rust programming language
+
+
+     // u8 , f32 , f64
+    println!("Hello, world!");
+
+    //  floating point 
+    // f64
+    let x = 2.0 ; 
+
+    // f32
+    let y : f32 = 3.0 
+
+    // addition
+    let sum = 5 + 10;
+
+    // substraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30 ;
+
+    // division
+    let quotient = 56.7 / 32.3 ;
+    let truncated = -5 / 3;
+
+    // remainder
+    let remainder = 43 % 5 ;
+
+    // boolean
+    let t = true;
+
+    let y:bool = false
+
+    // char
+    let c = 'z';
+    let z : char = 'Z';
+
+
+    // Tuple
+    let tup:(i32,f64,u8) = (500,5.6,1);
+
+
+    let tuplee = (500,6.4,1);
+
+    let (x,y,z) = tuplee;
+
+    println!("The value of y is:{y}")
+
+    let p :(132,f64,u8) = (500,6.4,1);
+
+    let five_hundred = p.0 ;
+
+    let six_point_four = p.1;
+
+    let one = x.2;
+
+
+    println!(five_hundred,six_point_four)
+
+    let a = [1 ,2 ,3 ,4]
+
+    let months = ["january","february","march","April","May","June"]
+}
