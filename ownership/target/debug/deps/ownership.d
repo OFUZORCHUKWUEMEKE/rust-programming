d91b27d26c809ca63c1950ad482b3a83ca3360f0
Ofuzor chukwuemeke <uniben2018@gmail.com>
2024-05-09 20:44:41 +0100

    ownership

---
+++ ownership/target/debug/deps/ownership.d
@@ -0,0 +1,5 @@
+C:\Users\OFUZOR IFEANYICHUKWU\Desktop\rust\ownership\target\debug\deps\ownership.exe: src\main.rs
+
+C:\Users\OFUZOR IFEANYICHUKWU\Desktop\rust\ownership\target\debug\deps\ownership.d: src\main.rs
+
+src\main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+C:\Users\OFUZOR\ IFEANYICHUKWU\Desktop\rust\ownership\target\debug\ownership.exe: C:\Users\OFUZOR\ IFEANYICHUKWU\Desktop\rust\ownership\src\main.rs
