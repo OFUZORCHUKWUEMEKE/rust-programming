ffea8c4010c933d4a9abbf67474f5109befdda54
Ofuzor chukwuemeke <uniben2018@gmail.com>
2024-05-10 18:17:12 +0100

    structs

---
+++ structexample/target/debug/deps/structexample.d
@@ -0,0 +1,5 @@
+C:\Users\OFUZOR IFEANYICHUKWU\Desktop\rust\structexample\target\debug\deps\structexample.exe: src\main.rs
+
+C:\Users\OFUZOR IFEANYICHUKWU\Desktop\rust\structexample\target\debug\deps\structexample.d: src\main.rs
+
+src\main.rs:
---
+++ structexample/target/debug/structexample.d
@@ -0,0 +1,1 @@
+C:\Users\OFUZOR\ IFEANYICHUKWU\Desktop\rust\structexample\target\debug\structexample.exe: C:\Users\OFUZOR\ IFEANYICHUKWU\Desktop\rust\structexample\src\main.rs
